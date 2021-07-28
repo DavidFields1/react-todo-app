b3f644823e2712ab20f69911cdc23b3f49687192
David <david.campos.solorio@gmail.com>
2021-07-27 20:55:07 -0500

    project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import TaskRow from './components/TaskRow'
+import TaskBanner from './components/TaskBanner';
+import TaskCreator from './components/TaskCreator'
+import VisiblityChange from './components/VisiblityChange';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  
+  const [userName, setUserName] = useState('');   
+  const [taskItems, setTaskItems] = useState([])
+  const [showCompleted, setShowCompleted] = useState(true)
+
+  useEffect(() => {
+    const data = localStorage.getItem('tasks');
+    if(data != null){
+      setTaskItems(JSON.parse(data))
+    } else {
+      setUserName('Admin')
+      setTaskItems([
+        {name: 'Task 11', done: false},
+        {name: 'Task 21', done: true},
+        {name: 'Task 31', done: true},
+        {name: 'Task 41', done: false}
+      ])
+      setShowCompleted(true)
+    }
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(taskItems))
+  }, [taskItems])
+
+  const createNewTask = (taskName) => {
+    if(!taskItems.find( task => task.name === taskName)){
+      setTaskItems([...taskItems,{name: taskName, done: false}])
+    }
+  }
+
+  const toggleTask = (task) => {
+    setTaskItems( taskItems.map ( t => ( t.name === task.name ? {...t, done: !t.done} : t)))
+  }
+
+  const taskPrinter = (doneValue) => (
+    taskItems
+    .filter( task => task.done === doneValue )
+    .map(task => (
+      <TaskRow task={task} key={task.name} toggleTask={toggleTask}/>
+    ))
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TaskBanner userName={userName} taskItems={taskItems}/>      
+      <div className="p-4">
+        <TaskCreator createNewTask={createNewTask}/>
+        <table className="table table-striped table-bordered table-hover mt-4">
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Done</th>
+            </tr>
+          </thead>
+          <tbody>
+            { taskPrinter(false) }
+          </tbody>
+        </table>
+        <div className="bg-secondary-text-white d-flex justify-content-center p-2">
+          <VisiblityChange isChecked={showCompleted} callback={checked => setShowCompleted(checked)}/>
+        </div>
+
+        {
+          showCompleted && (
+            <table className="table table-striped table-bordered table-hover">
+              <thead>
+                <tr>
+                  <th>Description</th>
+                  <th>Done</th>
+                </tr>
+              </thead>
+              <tbody>
+                {taskPrinter(true)}
+              </tbody>
+            </table>
+          )
+        }
+      </div>    
     </div>
   );
 }
